0954b0abcdba588792c6681514a9a176eb5b408e
DogsAreHumansToo <marc_laban@hotmail.com>
2023-06-20 04:18:18 +1200

    Portrait and text box finished

--- Assets/Scripts/PortraitChange.cs
+++ Assets/Scripts/PortraitChange.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PortraitChange : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public ResourceManager resourceManager;
+    public Image characterPortrait;
+    public Sprite Angry;
+    public Sprite Bored;
+    public Sprite Happy;
+    public Sprite Neutral;
+    public Sprite Sad;
+   
 
     // Update is called once per frame
-    void Update()
+  
+
+    public void ImageChange()
     {
-        
+        if (resourceManager.amount >= 50f)
+        {
+            characterPortrait.sprite = Neutral;
+        }
+        if (resourceManager.amount <= 25f)
+        {
+            characterPortrait.sprite = Happy;
+        }
+        if (resourceManager.amount >= 75f)
+        {
+            characterPortrait.sprite = Bored;
+        }
     }
 }
